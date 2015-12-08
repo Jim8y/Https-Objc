6e53554bae49b32e6242111160025e110488fdca
liaojinghui <liaojinghui@hnu.edu.cn>
2015-12-08 09:27:56 +0800

    Initial Commit

---
+++ HTTPS_TEST/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTPS_TEST
+//
+//  Created by 廖京辉 on 15/12/8.
+//  Copyright © 2015年 myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTTPS_TEST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTTPS_TEST
+//
+//  Created by 廖京辉 on 15/12/8.
+//  Copyright © 2015年 myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
